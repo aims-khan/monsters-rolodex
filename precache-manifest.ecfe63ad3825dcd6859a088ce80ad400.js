0c732a0afe8794db022b38a452d65badf46f8d14
aims-khan <zuhal.nader@gmail.com>
2020-10-07 18:37:50 +0430

    Updates

---
+++ precache-manifest.ecfe63ad3825dcd6859a088ce80ad400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41dc26a5be315978adc5a60837914dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "11031559fb54379e8729",
+    "url": "/monsters-rolodex/static/css/main.373f7b1a.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11031559fb54379e8729",
+    "url": "/monsters-rolodex/static/js/main.e03afe04.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
